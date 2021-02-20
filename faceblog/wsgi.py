fdf01ef4c755fa656a7fe7bc15783d257a8bbb0b
Konstantin Tornovskiy <coolship@yandex.ru>
2021-02-20 16:33:37 +0300

    Faceblog with blog application initial commit

---
+++ faceblog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for faceblog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faceblog.settings')
+
+application = get_wsgi_application()
